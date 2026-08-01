031ba3449cf5838f29212d529c8a33bc39911cd0
Ankit Singh <guypov7@gmail.com>
2026-08-01 11:42:43 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (98.26%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void sortColors(vector<int>& arr) {
+        int n = arr.size();
+        for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+
+        // Find the index of the smallest element
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        // Swap the smallest element with the current element
+        swap(arr[i], arr[minIndex]);
+    }
+    }
+};
